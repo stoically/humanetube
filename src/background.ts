aa4b86c5c572549e64d1c462544b8861a8d5a012
stoically <stoically@protonmail.com>
2020-10-31 11:04:42 +0100

    refactor: background

--- src/background.ts
+++ src/background.ts
@@ -1,41 +1,91 @@
-import { table } from "console";
+const EXTENSION_URL = browser.runtime.getURL("index.html");
 
-const extensionUrl = browser.runtime.getURL("/");
+interface WebRequestCtorArgs {
+  request: browser.webRequest._OnBeforeRequestDetails;
+  url: URL;
+  tab: browser.tabs.Tab;
+  openerTab?: browser.tabs.Tab;
+}
 
-browser.webRequest.onBeforeRequest.addListener(
-  async (request) => {
-    console.log(request.originUrl);
-    if (request.originUrl?.startsWith(extensionUrl)) {
+class WebRequest {
+  private readonly request: browser.webRequest._OnBeforeRequestDetails;
+  private readonly url: URL;
+  private readonly tab: browser.tabs.Tab;
+  private readonly openerTab?: browser.tabs.Tab;
+
+  constructor({ request, tab, url, openerTab }: WebRequestCtorArgs) {
+    this.request = request;
+    this.url = url;
+    this.tab = tab;
+    this.openerTab = openerTab;
+  }
+
+  static async factory(request: browser.webRequest._OnBeforeRequestDetails) {
+    const tab = await browser.tabs.get(request.tabId);
+    const url = new URL(request.url);
+    const openerTab =
+      (tab.openerTabId && (await browser.tabs.get(tab.openerTabId))) ||
+      undefined;
+
+    return new WebRequest({ request, url, tab, openerTab });
+  }
+
+  originatedFromExtension() {
+    return (
+      !!this.request.originUrl?.startsWith(EXTENSION_URL) ||
+      !!this.openerTab?.url?.startsWith(EXTENSION_URL)
+    );
+  }
+
+  parseYtIdFromUrl() {
+    let id;
+
+    if (this.url.hostname.endsWith(".youtube.com")) {
+      id = this.url.searchParams.get("v");
+    }
+
+    if (this.url.hostname.endsWith("youtu.be")) {
+      id = this.url.pathname.substring(1);
+    }
+
+    return id;
+  }
+
+  async handle() {
+    this.debug("handling", this.request);
+
+    if (this.originatedFromExtension()) {
       return {};
-    } else {
-      const tab = await browser.tabs.get(request.tabId);
-      if (tab.openerTabId) {
-        const openerTab = await browser.tabs.get(tab.openerTabId);
-        if (openerTab.url?.startsWith(extensionUrl)) {
-          return {};
-        }
+    }
+
+    const id = this.parseYtIdFromUrl();
+    if (id) {
+      const url = `${EXTENSION_URL}?watch=${id}`;
+
+      if (this.request.type === "main_frame") {
+        this.debug("creating tab", url);
+
+        browser.tabs.create({
+          url,
+          index: this.tab.index + 1,
+          windowId: this.tab.windowId,
+          openerTabId: this.openerTab?.id,
+        });
+        return { cancel: true };
       }
     }
 
-    const url = new URL(request.url);
-    let id;
-
-    if (url.hostname.endsWith(".youtube.com")) {
-      id = url.searchParams.get("v");
-    }
-
-    if (url.hostname.endsWith("youtu.be")) {
-      id = url.pathname.substring(1);
-    }
-
-    if (id) {
-      browser.tabs.create({
-        url: browser.extension.getURL(`index.html?watch=${id}`),
-      });
-      return { cancel: true };
-    }
-
     return {};
+  }
+
+  debug(message: string, ...args: unknown[]) {
+    console.log(`[request ${this.request.requestId}] ${message}`, ...args);
+  }
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  async (request) => {
+    return (await WebRequest.factory(request)).handle();
   },
   { urls: ["<all_urls>"], types: ["main_frame"] },
   ["blocking"]
