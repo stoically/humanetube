076ed100ae3a82647bef51e6c12651f1576a2293
stoically <stoically@protonmail.com>
2020-11-15 09:23:37 +0100

    feat: if we break completely, at least show the open-on-yt button

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,27 @@ import "rsuite/lib/styles/themes/dark/index.less";
 import React from "react";
 import ReactDOM from "react-dom";
 import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+import { Button } from "rsuite";
 import { Theater } from "./components/Theater";
 
 function ErrorFallback({ error }: FallbackProps) {
+  const id = new URL(document.location.toString()).searchParams.get("watch");
+
   return (
     <div role="alert">
       <p>Something went wrong:</p>
       <pre>{error?.message}</pre>
+      <div>
+        {id && (
+          <Button
+            href={`https://www.youtube.com/watch?v=${id}`}
+            target="_blank"
+            appearance="ghost"
+          >
+            Broken? Open on YouTube
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
